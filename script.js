09ca8dc9d32b9479be2bfc52cf7df2d1dd5a8d96
hartuuuu <animeloverjannacorro@gmail.com>
2026-07-30 21:08:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ===========================
+// GLVerse v0.1
+// ===========================
+
+console.log("Welcome to GLVerse 💜");
+
+// Future JavaScript features:
+// - Search
+// - Favorites
+// - Ratings
+// - Dark mode
+// - News slider
+// - Episode calendar
