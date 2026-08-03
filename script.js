864f73e979c795b3f11ef5c3e5a6eea59f3e2d24
hartuuuu <animeloverjannacorro@gmail.com>
2026-08-03 14:18:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// ===========================
-// GLHub v0.1
-// ===========================
 
-console.log("Welcome to GLHub 💜");
-
-// Future JavaScript features:
-// - Search
-// - Favorites
-// - Ratings
-// - Dark mode
-// - News slider
-// - Episode calendar
